25b0ddc67e2d761874f1c6c782f8fd7722e5b2f3
Franck Thang <stelyus@outlook.com>
2018-11-12 12:16:07 +0100

    Add reshaping for perfect modulo

--- jpeg/step.py
+++ jpeg/step.py
@@ -1,6 +1,6 @@
 import numpy as np
 from PIL import Image
-
+    
 QUANT_ = np.array(
   [[16,11,10,16,24,40,51,61],
   [12,12,14,19,26,58,60,55],
@@ -13,149 +13,184 @@ QUANT_ = np.array(
 )
 
 
-# Seems to be the first step
-def rgb_to_YCbCr(img_arr):
-  h = img_arr.shape[0]
-  w = img_arr.shape[1]
+
+class Jpeg():
+  def __init__(self, format=(4,2,0)):
+    dct = { (4,4,4): (8,8), (4,2,2): (16,8), (4,2,0): (16,16) }
+    
+    if format not in dct:
+      raise ValueError("Format %s not correct" % format)
+
+    self.mcu = dct[format]
+    self.format = format
+
+  def _reshaping(self, img_arr):
+    h = img_arr.shape[0]
+    w = img_arr.shape[1]
+
+    ah = h % self.mcu[0]
+    aw = w % self.mcu[1]
+      
+    #print("Module ah %s, aw %s" % (ah, aw))
+    #print("Shape image %s" % (img_arr.shape,))
+    #print("Last right shape %s" % (img_arr[:,-1].shape,))
+    #print("Last end shape %s" % (img_arr[-1].shape,))
+
+    arr_h = np.full((ah, w, 3), img_arr[-1])
+    new_arr = np.vstack((img_arr, arr_h))
+    arr_w = np.full((h + ah, 3), new_arr[:,-1]).reshape((h + ah,aw,3))
+    new_arr = np.hstack((new_arr, arr_w))
+
+    return new_arr
+
+  def _rgb_to_YCbCr(self, img_arr):
+    # Seems to be the first step
+    h = img_arr.shape[0]
+    w = img_arr.shape[1]
+
+    y_coeff = np.full(img_arr.shape,np.asarray([(77/256),(150/256),(29/256)]))
+    cb_coeff = np.full(img_arr.shape,np.asarray([-(44/256),-(87/256),(131/256)]))
+    cr_coeff = np.full(img_arr.shape,np.asarray([(131/256),- (110/256),-(21/256)]))
+    bias = np.asarray([0,128,128])
+
+    Y_tmp =  np.multiply(y_coeff,img_arr)
+    Y = np.sum(Y_tmp, axis=-1).reshape((h,w,1))
+
+    Cb_tmp =  np.multiply(cb_coeff,img_arr)
+    Cb = np.sum(Cb_tmp,axis=-1).reshape((h,w,1))
+
+    Cr_tmp =  np.multiply(cr_coeff,img_arr)
+    Cr = np.sum(Cr_tmp,axis=-1).reshape((h,w,1))
+
+    new = np.concatenate([Y, Cb, Cr], axis=-1) + bias
+
+    return new 
 
 
-  y_coeff = np.full(img_arr.shape,np.asarray([(77/256),(150/256),(29/256)]))
-  cb_coeff = np.full(img_arr.shape,np.asarray([-(44/256),-(87/256),(131/256)]))
-  cr_coeff = np.full(img_arr.shape,np.asarray([(131/256),- (110/256),-(21/256)]))
-  bias = np.asarray([0,128,128])
+  def _chroma_subsampling(self, img_arr):
+    # Seems to be the second step
+    # Depending on chroma subsampling, this yields Minimum Coded Unit (MCU) blocks
+    # of size 8×8 (4:4:4 – no subsampling), 16×8 (4:2:2), or most commonly 16×16
+    # (4:2:0)
+    # the encoder must fill the remaining area of the incomplete blocks with some
+    # form of dummy data.
+    # Seems to be the third step
 
-  Y_tmp =  np.multiply(y_coeff,img_arr)
-  Y = np.sum(Y_tmp, axis=-1).reshape((h,w,1))
+    def remove_chroma(array, pattern_width):
+      if pattern_width == 4:
+        return array
 
-  Cb_tmp =  np.multiply(cb_coeff,img_arr)
-  Cb = np.sum(Cb_tmp,axis=-1).reshape((h,w,1))
+      w = array.shape[0]
+      pattern_width_dict = {0: w+1, 1: 4, 2: 2}
+      step = pattern_width_dict[pattern_width]
 
-  Cr_tmp =  np.multiply(cr_coeff,img_arr)
-  Cr = np.sum(Cr_tmp,axis=-1).reshape((h,w,1))
-
-  new = np.concatenate([Y, Cb, Cr], axis=-1) + bias
-
-  return new 
+      for i in range(0,w):
+        if i % step != 0:
+          array[i] = [array[i][0], 0, 0]
 
 
+      # If the pattern is 0 we need to remove the first index 
+      if step == pattern_width_dict[0]:
+        array[0] = [array[i][0], 0, 0]
+
+
+    step_width = 2
+    
+    h = img_arr.shape[0]
+    for i in range(0,h):
+      th = img_arr[i]
+      if i % step_width == 0:
+        remove_chroma(th, self.format[0])
+      else:
+        remove_chroma(th, self.format[1])
+      img_arr[i] = th 
+
+
+   
+
+  def _dct_transformation(self, img_arr):
+    def DCT(i, j, img_arr):
+      CI = 1/np.sqrt(2) if i == 0 else 1
+      CJ = 1/np.sqrt(2) if j == 0 else 1
+    
+      
+      total = 0
+      for x in range(0,8):
+        min_total = 0
+        for y in range(0,8):
+          cos = np.cos([((2*x+1)*i*np.pi) / 16,((2*y+1)*j*np.pi) / 16])
+          min_total += img_arr[x,y] * cos[0] * cos[1]
+        total += min_total
+
+      return np.round(2/8 * CI * CJ * total)
+
+
+    if img_arr.shape != (8,8):
+      raise ValueError("Img array shape must be 8x8")
+
+    dct_transformed = np.copy(img_arr)
+
+    for i in range(0,8):
+      for j in range(0,8):
+        value_pixel = img_arr[i,j]
+        dct_transformed[i, j] = DCT(i, j, img_arr) 
+
+    return dct_transformed
+
+
+  # Seems to be the fourth step
+  def _quantification(self, dct_arr):
+    #quant_matrix = np.fromfunction(lambda i,j: 1+(i+j+1)*quality, (8,8))
+    quantified_matrix = np.copy(dct_arr)
+
+    for u in range(0,8):
+      for v in range(0,8):
+        quantified_matrix[u,v] = (dct_arr[u,v] + (QUANT_[u,v] // 2)) // QUANT_[u,v]
+
+    return quantified_matrix
+
+
+  # Seems to be the fifth step
+  def _codage(self, quant_m):
+    diag_values = []
+    for x in range(1, 9):
+      diag = list(zip(range(0,x), range(x-1,-1,-1)))
+      if x % 2:
+        diag = diag[::-1]
+      for pos in diag:
+        diag_values.append(quant_m[pos[0], pos[1]])
+
+
+    for x in range(1,8):
+      diag = list(zip(range(x,8), range(7,-1,-1)))
+      if x % 2:
+        diag = diag[::-1]
+      for pos in diag:
+        diag_values.append(quant_m[pos[0], pos[1]])
+    
+    idx_l = len(diag_values) \
+            - next(i for i,v in enumerate(diag_values[::-1]) if v != 0)
+    return diag_values[0:idx_l]
+
+
+
+
+  def _compress(self, diag_l):
+    pass
+    
+
+  def run(self, imgpath):
+    img = Image.open(imgpath)
+    reshaped_image = self._reshaping(np.asarray(img))
+    #new_image = self._rgb_to_YCbCr(np.asarray(img))
+    #self._chroma_subsampling(new_image)
+    #dct_transformed = self._dct_transformation(dct_test)
+    #quant_m = self._quantification(dct_transformed)
+    #print(self._codage(quant_m))
+#
 
 #def YCbCr_to_rgb():
   #R = Y + 1.402 * (Cr-128)
   #G = Y - 0.34414 * (Cb - 128) - 0.71414 * (Cr - 128)
   #B = Y + 1.772 * (Cb - 128)
   #return R, G, B
-
-
-
-# Seems to be the second step
-def chroma_subsampling(img_arr,a=2,b=0): 
-
-  def remove_chroma(array, pattern_width):
-    if pattern_width == 4:
-      return array
-
-    w = array.shape[0]
-    pattern_width_dict = {0: w+1, 1: 4, 2: 2}
-    step = pattern_width_dict[pattern_width]
-
-    for i in range(0,w):
-      if i % step != 0:
-        array[i] = [array[i][0], 0, 0]
-
-
-    # If the pattern is 0 we need to remove the first index 
-    if step == pattern_width_dict[0]:
-      array[0] = [array[i][0], 0, 0]
-
-
-  step_width = 2
-  
-  h = img_arr.shape[0]
-  for i in range(0,h):
-    th = img_arr[i]
-    remove_chroma(th, a) if i % step_width == 0 else remove_chroma(th, b)
-    img_arr[i] = th 
-
-
-# Seems to be the third step
-def dct_transformation(img_arr):
-  def DCT(i, j, img_arr):
-    CI = 1/np.sqrt(2) if i == 0 else 1
-    CJ = 1/np.sqrt(2) if j == 0 else 1
-  
-    
-    total = 0
-    for x in range(0,8):
-      min_total = 0
-      for y in range(0,8):
-        cos = np.cos([((2*x+1)*i*np.pi) / 16,((2*y+1)*j*np.pi) / 16])
-        min_total += img_arr[x,y] * cos[0] * cos[1]
-      total += min_total
-
-    return np.round(2/8 * CI * CJ * total)
-
-
-  if img_arr.shape != (8,8):
-    raise ValueError("Img array shape must be 8x8")
-
-  dct_transformed = np.copy(img_arr)
-
-  for i in range(0,8):
-    for j in range(0,8):
-      value_pixel = img_arr[i,j]
-      dct_transformed[i, j] = DCT(i, j, img_arr) 
-
-  return dct_transformed
-
-
-
-# Seems to be the fourth step
-def quantification(dct_arr):
-  #quant_matrix = np.fromfunction(lambda i,j: 1+(i+j+1)*quality, (8,8))
-  quantified_matrix = np.copy(dct_arr)
-
-  for u in range(0,8):
-    for v in range(0,8):
-      quantified_matrix[u,v] = (dct_arr[u,v] + (QUANT_[u,v] // 2)) // QUANT_[u,v]
-
-  return quantified_matrix
-
-# Seems to be the fifth step
-def codage(quant_m):
-  diag_values = []
-  for x in range(1, 9):
-    diag = list(zip(range(0,x), range(x-1,-1,-1)))
-    if x % 2:
-      diag = diag[::-1]
-    for pos in diag:
-      diag_values.append(quant_m[pos[0], pos[1]])
-
-
-  for x in range(1,8):
-    diag = list(zip(range(x,8), range(7,-1,-1)))
-    if x % 2:
-      diag = diag[::-1]
-    for pos in diag:
-      diag_values.append(quant_m[pos[0], pos[1]])
-  
-  idx_l = len(diag_values) \
-          - next(i for i,v in enumerate(diag_values[::-1]) if v != 0)
-  return diag_values[0:idx_l]
-  
-
-
-path = "/Users/franckthang/Work/PersonalWork/jpeg-compression/resources/cat.jpg"
-img = Image.open(path)
-new_image = rgb_to_YCbCr(np.asarray(img))
-
-chroma_subsampling(new_image)
-
-
-dct_test = np.array([[139,144,149,153,155,155,155,155],
-[144,151,153,156,159,156,156,156], [150,155,160,163,158,156,156,156],
-[159,161,162,160,160,159,159,159],[159,160,161,162,162,155,155,155],[161,161,161,161,160,157,157,157],[162,162,161,163,162,157,157,157],[162,162,161,161,163,158,158,158]])
-
-dct_transformed = dct_transformation(dct_test)
-#print(dct_transformed)
-quant_m = quantification(dct_transformed)
-codage(quant_m)
